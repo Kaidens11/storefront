f96926162943bf4f6ade34b92128233f54faaba9
Kaidens11 <gerardo_solar.e@hotmail.com>
2026-08-14 02:26:07 -0500

    feat(store): personalizar tabla Customer y limpiar proyecto
    - Agregado db_table='store_customers' e indices en el modelo Customer.
    - Generadas y aplicadas las migraciones de la base de datos.
    - Agregado archivo .gitignore para omitir archivos .pyc, __pycache__ y db.sqlite3.

--- store/models.py
+++ store/models.py
@@ -2,19 +2,21 @@ from django.db import models
 
 class Promotion(models.Model):
     description = models.CharField(max_length=255)
-    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
+    discount = models.FloatField()
 
 class Collection(models.Model):
     title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
-    promotion = models.ManyToManyField(Promotion)
+    promotions = models.ManyToManyField(Promotion)
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
@@ -58,6 +60,7 @@ class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    zip = models.CharField(max_length=255)
 
 class Cart(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
